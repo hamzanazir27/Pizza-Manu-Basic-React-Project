ee25e6afa07a3816ec17b755f9feb600aa039579
hamzanazir27 <hamzaramay51@gmail.com>
2025-03-17 22:15:50 +0500

    manu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from "react"; //
 import ReactDOM from "react-dom/client"; //
 import "./style.css";
+/////////////////////////////
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+////////////////////////////
+
 function App() {
   return (
     <div className="container">
       <Header />
-      {/* <Manu />
-     <footer />  */}
+      <Manu />
     </div>
   );
 }
@@ -18,6 +65,17 @@ function Header() {
     </header>
   );
 }
+function Manu()
+{
+   return(
+   <div className="menu">
+    <h2>Our menu</h2>
+    <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.
+    </p>
+   </div>);
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
